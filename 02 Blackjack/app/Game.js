85b28d3772632a6a6c88c8f9123b008da451aada
Sławomir Pietrzak <1pietrzakslawomir@gmail.com>
2023-01-30 07:06:44 +0100

    Add Game

--- 02 Blackjack/app/Game.js
+++ 02 Blackjack/app/Game.js
@@ -1,13 +1,36 @@
 import { Deck } from './Deck.js';
-
+import { Player } from './Player.js';
 class Game {
-  constructor() {
-
+  constructor({ playerName, playerCards, dealerCards }) {
+    this.player = new Player(playerName);
+    this.dealer = new Player('Dealer');
+    this.playerCards = playerCards;
+    this.dealerCards = dealerCards;
+    this.deck = new Deck();
+    this.deck.shuffle();
   }
-  run() {
 
+  run() {
+    this.dealCards();
+  }
+
+  dealCards() {
+    for (let i = 0; i < 2; i++) {
+      let playerCard = this.deck.pickOne();
+      this.player.hand.addCardToHand(playerCard);
+      this.playerCards.appendChild(playerCard.render());
+
+      let dealerCard = this.deck.pickOne();
+      this.dealer.hand.addCardToHand(dealerCard);
+      this.dealerCards.appendChild(dealerCard.render());
+    }
   }
 }
 
+const game = new Game({
+  playerName: 'Sławek',
+  playerCards: document.getElementById('playersCards'),
+  dealerCards: document.getElementById('dealersCards'),
+})
 
-const player = new Player('slawek');
+game.run();
