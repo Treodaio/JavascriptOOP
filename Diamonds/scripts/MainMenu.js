9c15d47dd93967f97eaff4d51cbc21621d104e66
Sławomir Pietrzak <1pietrzakslawomir@gmail.com>
2023-02-04 17:08:38 +0100

    Add resizing to start screen

--- Diamonds/scripts/MainMenu.js
+++ Diamonds/scripts/MainMenu.js
@@ -1,5 +1,6 @@
 import { Common, HIDE_SCREEN, SHOW_SCREEN } from './Common.js';
 
+const SCALE_PROPERTY = '--scale-value';
 const START_SCREEN_ID = 'js-start-screen';
 const START_SCREEN_NEW_GAME_BUTTON_ID = 'js-start-game';
 const START_SCREEN_SETTINGS_BUTTON_ID = 'js-settings-button';
@@ -8,14 +9,16 @@ class MainMenu extends Common {
   constructor() {
     super(START_SCREEN_ID);
     this.bindToGameElements();
+    this.resizeGameWindow();
+    window.addEventListener('resize', this.resizeGameWindow);
   }
 
   bindToGameElements() {
     const gameStartButton = this.bindToElement(START_SCREEN_NEW_GAME_BUTTON_ID);
     const gameSettingsButton = this.bindToElement(START_SCREEN_SETTINGS_BUTTON_ID);
 
-    gameStartButton.addEventListener('click', () => { this.showLevel(); });
-    gameSettingsButton.addEventListener('click', () => { this.showSettingsScreen(); })
+    gameStartButton.addEventListener('click', this.showLevel);
+    gameSettingsButton.addEventListener('click', this.showSettingsScreen);
   }
 
   showLevel() {
@@ -24,7 +27,12 @@ class MainMenu extends Common {
 
   showSettingsScreen() {
     console.log('new start');
+  }
 
+  resizeGameWindow() {
+    const { innerWidth: width, innerHeight: height } = window;
+    const scaleValue = Math.min(width / 640, height / 480);
+    document.documentElement.style.setProperty(SCALE_PROPERTY, scaleValue);
   }
 }
 
