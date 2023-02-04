9e8031885c5f28c435d934f0cc28419bc1edeb66
Sławomir Pietrzak <1pietrzakslawomir@gmail.com>
2023-02-04 16:58:06 +0100

    Add initial methods

--- Diamonds/scripts/MainMenu.js
+++ Diamonds/scripts/MainMenu.js
@@ -1,6 +1,31 @@
-import { Common } from './Common.js';
-class MainMenu {
+import { Common, HIDE_SCREEN, SHOW_SCREEN } from './Common.js';
 
+const START_SCREEN_ID = 'js-start-screen';
+const START_SCREEN_NEW_GAME_BUTTON_ID = 'js-start-game';
+const START_SCREEN_SETTINGS_BUTTON_ID = 'js-settings-button';
+
+class MainMenu extends Common {
+  constructor() {
+    super(START_SCREEN_ID);
+    this.bindToGameElements();
+  }
+
+  bindToGameElements() {
+    const gameStartButton = this.bindToElement(START_SCREEN_NEW_GAME_BUTTON_ID);
+    const gameSettingsButton = this.bindToElement(START_SCREEN_SETTINGS_BUTTON_ID);
+
+    gameStartButton.addEventListener('click', () => { this.showLevel(); });
+    gameSettingsButton.addEventListener('click', () => { this.showSettingsScreen(); })
+  }
+
+  showLevel() {
+    console.log('Start');
+  }
+
+  showSettingsScreen() {
+    console.log('new start');
+
+  }
 }
 
 export const mainMenu = new MainMenu(); 
