d176018e74dbb2eefbc6f37c90e6e3f02fbe35db
Sławomir Pietrzak <1pietrzakslawomir@gmail.com>
2023-02-04 16:56:56 +0100

    Rename Common method

--- Diamonds/scripts/Common.js
+++ Diamonds/scripts/Common.js
@@ -4,15 +4,16 @@ export const HIDDEN_CLASS = 'hidden';
 
 export class Common {
   constructor(elementId) {
-    this.element = this.bindElement(elementId);
+    this.element = this.bindToElement(elementId);
   }
 
   toggleElementVisibility(element, mode) {
     mode === SHOW_SCREEN ? element.classList.remove(HIDDEN_CLASS) : element.classList.add(HIDDEN_CLASS);
   }
 
-  bindElement(elementId) {
-    const element = document.querySelector(`#${elementId}`);
+  bindToElement(ID) {
+    const element = document.getElementById(ID);
+
     if (!element) {
       throw new Error(`Nie znaleziono elementu o ID ${elementId}`);
     }
